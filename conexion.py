f4a7f7ee54abf026e5c598ad1c09feb42345af23
SanDroiD07 <ramosg.sandro@gmail.com>
2024-04-17 09:19:03 -0500

    Guarda los cambios actuales

--- conexion.py
+++ conexion.py
@@ -4,9 +4,9 @@ import os
 def create_connection():
     try:
         connection = connect(
-            host=os.getenv("DB_SERVERNAME"),
-            user=os.getenv("DB_USERNAME"),
-            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
             database=os.getenv("DB_NAME"),
         )
         print("Conexión exitosa a la base de datos")
